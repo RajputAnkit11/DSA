6bb0882f4412d8c196dc4acb31101dfe5b116b21
Ankit Singh <56732288+RajputAnkit11@users.noreply.github.com>
2022-10-03 18:07:35 +0530

    Update Two Sum.cpp

--- Hash Map/Two Sum.cpp
+++ Hash Map/Two Sum.cpp
@@ -1,9 +1,13 @@
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// You can return the answer in any order.
+
 
 // Hash Map - O(NlongN)
 vector<int> twoSum(vector<int>& nums, int target) {
     int n = nums.size();
     unordered_map<int,int> map;
-
+    
     for(int i=0;i<n;i++){
         int temp = target - nums[i];
 
