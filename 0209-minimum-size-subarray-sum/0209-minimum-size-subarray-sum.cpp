e61d918fb739f512bb6e1400f9b4f81329683ba4
Ankit Singh <56732288+RajputAnkit11@users.noreply.github.com>
2022-10-16 22:28:23 +0530

    Time: 89 ms (35.28%), Space: 28.2 MB (52.80%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -3,21 +3,10 @@ public:
     int minSubArrayLen(int target, vector<int>& nums) {
         
         int n = nums.size();
-        
         int l = 0, r = 0;
         long long int sum = 0;
         int ans = INT_MAX;
-        
-        // while(r<n){
-        //     sum += nums[r];
-        //     r++;
-        //     while(sum>=target){
-        //         sum -= nums[l];
-        //         ans = min(ans,r-l+2);
-        //         l--;
-        //     }
-        // }
-        
+         
         while(true){
             bool f1 = false;
             bool f2 = false;
@@ -25,22 +14,20 @@ public:
                 f1 = true;
                 sum += nums[r];
                 r++;
-                cout<<sum<<endl;
                 if(sum>=target){
                     break;
                 }
                 f1 = false;
             }
-            cout<<"here\n";
+            
             while(l<r){
                 if(f1 == false) break;
                 f2 = true;
                 sum -= nums[l];
-                cout<<"sum 2 = "<<sum<<endl;
+                
                 l++;
                 if(sum<target){
                     ans = min(ans,r-l+1);
-                    cout<<"ans "<<ans<<endl;
                     break;
                 }
                 else{
