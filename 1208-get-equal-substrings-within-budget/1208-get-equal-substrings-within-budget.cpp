8beb8b5ea259cb01f05030cd985ed990e4afb93d
Ankit Singh <56732288+RajputAnkit11@users.noreply.github.com>
2022-10-21 13:47:03 +0530

    Time: 12 ms (56.40%), Space: 8.3 MB (12.43%) - LeetHub

---
+++ 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int largestSubArrayTargetSum(vector<int> &nums, int target){
+        int n = nums.size();
+        
+        int ans = 0;
+        
+        int i=0;
+        int j=0;
+        
+        int sum = 0;
+        while(j<n){
+            sum += nums[j];
+            j++;
+            while(sum>target){
+                sum -= nums[i];
+                i++;
+            }
+            if(sum <= target){
+                ans = max(ans,j-i);    
+            }
+        }
+        return ans;
+    }
+    
+    int equalSubstring(string s, string t, int maxCost) {
+        
+        int n = s.length();
+        
+        vector<int> nums(n);
+        for(int i=0;i<n;i++){
+            nums[i] = abs(s[i]-t[i]);
+        }
+        
+        return largestSubArrayTargetSum(nums,maxCost);
+    }
+};
