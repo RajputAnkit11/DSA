3ba12c628c1622d6f7c978c84a330ab037704f58
Ankit Singh <56732288+RajputAnkit11@users.noreply.github.com>
2022-10-29 16:07:07 +0530

    Time: 19 ms (33.54%), Space: 13 MB (59.69%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
+    // memoization
     int helper(int i,int canBuy, vector<int>&prices, vector<vector<int>>&dp){
-        
         if(i == prices.size()){
             return 0;
         }
-        
         if(dp[i][canBuy] != -1) return dp[i][canBuy];
         
         int profit = INT_MIN;
@@ -19,7 +18,6 @@ public:
             profit = max( prices[i] + helper(i+1,1,prices,dp),
                           helper(i+1,0,prices,dp) );
         }
-        
         return dp[i][canBuy] = profit;
     }
     
@@ -30,6 +28,7 @@ public:
         // int canBuy = 1; 
         // return helper(0,canBuy,prices,dp);
         
+        /* tabulation
         int n = prices.size();
         vector<vector<int>> dp(n+1,vector<int>(2,-1));
         dp[n][0] = dp[n][1] = 0;
@@ -48,6 +47,45 @@ public:
         }
         
         return dp[0][1];
+        */
+        
+        /* space optimization - 1
+        int n = prices.size();
+        vector<int> ahead (2,0);
+        vector<int> cur(2,0);
+        ahead[0] = ahead[1] = 0;
+
+        int profit;
+        for(int ind= n-1; ind>=0; ind--){
+            for(int buy=0; buy<=1; buy++){
+                if(buy==0){// We can buy the stock
+                    profit = max(0+ahead[0], -prices[ind] + ahead[1]);
+                }
+
+                if(buy==1){// We can sell the stock
+                    profit = max(0+ahead[1], prices[ind] + ahead[0]);
+                }
+                cur[buy]  = profit;
+            }
+
+            ahead = cur;
+        }
+        return cur[0];
+        */
+        
+        // space optimization - 2
+        int n = prices.size();
+        int aheadNotBuy = 0, aheadBuy = 0;
+        int curNotBuy, curBuy;
+        
+        for(int i= n-1; i>=0; i--){
+            curNotBuy = max( prices[i] + aheadBuy, aheadNotBuy );
+            curBuy = max( -prices[i] + aheadNotBuy, aheadBuy );
+            
+            aheadNotBuy = curNotBuy;
+            aheadBuy = curBuy;
+        }
+        return aheadBuy;
     }
     
 };
