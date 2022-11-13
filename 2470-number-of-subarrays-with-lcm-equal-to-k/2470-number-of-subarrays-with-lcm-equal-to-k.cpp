c70c057fb8f096cd1889e67cba9d2085e7b5210a
Ankit Singh <56732288+RajputAnkit11@users.noreply.github.com>
2022-11-13 09:38:13 +0530

    Time: 246 ms (12.50%), Space: 9.2 MB (50.00%) - LeetHub

--- 2470-number-of-subarrays-with-lcm-equal-to-k/2470-number-of-subarrays-with-lcm-equal-to-k.cpp
+++ 2470-number-of-subarrays-with-lcm-equal-to-k/2470-number-of-subarrays-with-lcm-equal-to-k.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    unsigned int gcd(unsigned int a, unsigned int b)
+    int gcd(unsigned int a, unsigned int b)
     {
       if (b == 0)
         return a;
       return gcd(b, a % b);
     }
 
-    unsigned int lcm(unsigned int a, unsigned int b)
+    int lcm(unsigned int a, unsigned int b)
     {
         return (a / gcd(a, b)) * b;
     }
