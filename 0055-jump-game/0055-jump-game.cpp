473521ed397f6c8056a60c6bb2fd14126aca1824
Ankit Singh <56732288+RajputAnkit11@users.noreply.github.com>
2022-12-26 22:08:44 +0530

    Time: 1731 ms (8.73%), Space: 51.4 MB (6.72%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    
     bool solve(vector<int>& nums,int i,vector<int>&dp)
     {
         if(i>=nums.size()-1) return true;
@@ -10,12 +11,12 @@ public:
         
         for(int j=1;j<=nums[i];j++){
             
-            if(solve(nums,i+j,dp))
-                return dp[i]=true;
+            if(solve(nums,i+j,dp)) return dp[i]=true;
         }
         
         return dp[i]=false;
     }
+    
     bool canJump(vector<int>& nums) {
         
         vector<int>  dp(nums.size(),-1);
