5995ad66157bbab574b2e17b373207eb50027acc
Ankit Singh <56732288+RajputAnkit11@users.noreply.github.com>
2022-12-30 21:44:37 +0530

    Time: 7 ms (99.14%), Space: 10.6 MB (83.12%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
@@ -5,17 +5,12 @@ public:
         if(i == n-1){
             ans.push_back(temp);
         }
-        
         if(i == 0){
             temp.push_back(0);
         }
-        
-        for(int it=0;it<graph[i].size();it++){
-            
-            temp.push_back(graph[i][it]);
-            
-            dfs(graph[i][it],n,temp,ans,graph);
-            
+        for(auto &it: graph[i]){
+            temp.push_back(it);
+            dfs(it,n,temp,ans,graph);
             temp.pop_back();
         }
         return ;
