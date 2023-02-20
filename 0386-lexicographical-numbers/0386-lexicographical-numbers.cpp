8eb78427104d5c5d6e2b7aa8e0ffae5af527168a
Ankit Singh <56732288+RajputAnkit11@users.noreply.github.com>
2023-02-20 10:26:46 +0530

    Time: 10 ms (73.05%), Space: 11.5 MB (58.63%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -1,19 +1,26 @@
 class Solution {
 public:
+    
+    void dfs(int cur, int n, vector<int> &ans){
+        if(cur > n){
+            return ;
+        }
+        else{
+            ans.push_back(cur);
+            for(int i=0;i<10;i++){
+                int x = cur*10 + i;
+                
+                if(x>n) return ;
+                else dfs(x,n,ans);
+            }
+        }
+    }
+    
     vector<int> lexicalOrder(int n) {
         
         vector<int> ans;
-        vector<string> v;
-        for(int i=1;i<=n;i++){
-            string s = to_string(i);
-            v.push_back(s);
-        }
-        
-        sort(v.begin(),v.end());
-        
-        for(string s : v){
-            int x = stoi(s);
-            ans.push_back(x);
+        for(int i=1;i<=9;i++){
+            dfs(i,n,ans);
         }
         
         return ans;
