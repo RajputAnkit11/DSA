c85edbc44cb67f77f6c4a2dda6f48396b534ab0d
Ankit Singh <56732288+RajputAnkit11@users.noreply.github.com>
2023-06-26 10:55:29 +0530

    Time: 8 ms (38.25%), Space: 12.7 MB (23.28%) - LeetHub

--- 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.cpp
+++ 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.cpp
@@ -20,17 +20,12 @@ public:
         while(q.size()>0){
             vector<int> v;
             int size = q.size();
-            
-            for (int i = 0; i < size;i++){
+            for(int i = 0; i < size;i++){
                 TreeNode *temp = q.front();
                 q.pop();
                 v.push_back(temp->val);
-                if(temp->left){
-                    q.push(temp->left);
-                }
-                if(temp->right){
-                    q.push(temp->right);
-                }
+                if(temp->left)  q.push(temp->left);
+                if(temp->right) q.push(temp->right);
             }
             ans.push_back(v);
         }
