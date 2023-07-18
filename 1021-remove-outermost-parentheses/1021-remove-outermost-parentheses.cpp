bb706fd2131af9326e6f60a89fd0025aac20b8df
Ankit Singh <56732288+RajputAnkit11@users.noreply.github.com>
2023-07-18 23:11:55 +0530

    Time: 4 ms (48.53%), Space: 8.2 MB (11.98%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -1,29 +1,46 @@
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-        stack<char> st;
+        // stack<char> st;
         string ans="";
+        int n = s.size();
+//         for(int i=0;i<s.size();i++){
 
-        for(int i=0;i<s.size();i++){
-
-            if(s[i]=='(' && st.empty()){
-                st.push(i);
-            }
-            else if(s[i]=='(' && !st.empty()){
-                st.push(s[i]);
-                ans+=s[i];
+//             if(s[i]=='(' && st.empty()){
+//                 st.push(i);
+//             }
+//             else if(s[i]=='(' && !st.empty()){
+//                 st.push(s[i]);
+//                 ans+=s[i];
+//             }
+//             else{
+//                 st.pop();
+//                 if(!st.empty()) ans+=s[i];
+//             }
+//         }
+        
+        
+        vector<pair<int,char>> v;
+        int c = 0;
+        for(int i=0;i<n;i++){
+            if(s[i] == '('){
+                c++;
+                v.push_back({c,'('});
             }
             else{
-                st.pop();
-                if(!st.empty()) ans+=s[i];
+                v.push_back({c,')'});
+                c--;
+            }
+        }
+        
+        for(int i=0;i<n;i++){
+            // cout<<v[i].first<<" "<<v[i].second<<"\n";
+            if(v[i].first != 1){
+                ans += v[i].second;
             }
         }
 
-        // for(int i=0;i<helper.size();i++){
-        //     cout << helper[i] << " ";
-        // }
-
-
+        
         return ans;
     }
 };
